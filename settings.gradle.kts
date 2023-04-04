317c12844338eced621a80a180314d71948c3a80
GitHub Action <action@github.com>
2023-04-04 10:12:19 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "wh-template-playground"
--- src/main/kotlin/com/github/thanhhavan/whtemplateplayground/MyBundle.kt
+++ src/main/kotlin/com/github/thanhhavan/whtemplateplayground/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.thanhhavan.whtemplateplayground
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
